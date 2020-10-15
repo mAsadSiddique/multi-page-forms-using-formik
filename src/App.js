564337657ac26e3e007d1e22f0e0cf2150a00c63
mAsadSiddique <asadsiddique.a@gmail.com>
2020-10-15 23:10:08 +0500

    Create form-1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BasicTextFields from './Components/form1';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicTextFields />
+    </div>
+  );
+}
+
+export default App;
